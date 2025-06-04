9a78a3d798c7a820b93f1645df4100c4bfafc188
erikseptian09 <121048520+erikseptian09@users.noreply.github.com>
2025-06-04 19:02:02 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('app-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/data_pelanggan.js',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
